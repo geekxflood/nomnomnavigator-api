0dab5a02e6e3f09bfcc434ce848dd3a65f5a80aa
Christophe Rime <christopherime@me.com>
2023-10-20 20:56:20 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,127 @@
-# nomnomnavigator-api
-A whimsical API to fetch and serve daily meal menus from the company restaurant.
+# 🍽️ NomNomNavigator
+
+Ahoy there, brave digital voyagers! Welcome aboard the SS NomNomNavigator, a gallant vessel sailing the turbulent seas of the web in quest for scrumptious sustenance! With a map of allergens in one hand and a spoon in the other, we're on a grand expedition to fetch the fabled menu data from the mysterious Restaurant Isles. 🏴‍☠️🦜
+
+## Table of Contents
+
+- [🍽️ NomNomNavigator](#️-nomnomnavigator)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [API](#api)
+    - [Get Menu](#get-menu)
+  - [Development](#development)
+  - [License](#license)
+
+## Installation
+
+Before we set sail, ensure ye have [Node.js](https://nodejs.org/en/) installed in yer system.
+
+1. Clone this repository to yer local machine:
+
+    ```bash
+    git clone https://github.com/your-username/nomnomnavigator.git
+    cd nomnomnavigator
+    ```
+
+2. Install the necessary provisions (dependencies) for our voyage:
+
+    ```bash
+    npm install
+    ```
+
+3. Hoist the sails and set the environment variable for the restaurant website:
+
+    ```bash
+    export RESTAURANT_WEBSITE='https://your-restaurant-url.com'
+    ```
+
+## Usage
+
+1. With the preparations complete, it's time to set sail! Launch the server with the following command:
+
+    ```bash
+    npm start
+    ```
+
+2. The SS NomNomNavigator is now simmering on port 3000. Access the bounteous banquet of data at:
+
+    ```http
+    GET http://localhost:3000/menu
+    ```
+
+## API
+
+### Get Menu
+
+Fetches the fabled menu data from the Restaurant Isles.
+
+- **URL:** `/menu`
+- **Method:** `GET`
+- **Success Response:**
+    - **Code:** 200 OK
+    - **Content:**
+        ```json
+        [
+            {
+                "date": "YYYY-MM-DD",
+                "meals": [
+                    {
+                        "title": "String",
+                        "description": "String",
+                        "price": "Number",
+                        "currency": "String",
+                        "allergens": [
+                            {
+                                "id": "Number",
+                                "name": "String",
+                                "emoji": "String"
+                            }
+                        ],
+                        "nutritionalInfo": [
+                            {
+                                "name": "String",
+                                "value": "Number",
+                                "percentage": "Number"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+        ```
+
+## Development
+
+Should ye wish to venture into the codebase and contribute to the grand expedition, follow these steps:
+
+1. Fork the repository to yer own account and clone it to yer local machine.
+2. Install the provisions:
+    ```bash
+    npm install
+    ```
+
+3. Commence tinkering! Create a new branch for yer feature or bug fix:
+    ```bash
+    git checkout -b my-awesome-feature
+    ```
+
+4. Make yer changes and commit them with a hearty message:
+    ```bash
+    git commit -am "Add some awesome feature"
+    ```
+
+5. Push yer changes to GitHub:
+    ```bash
+    git push origin my-awesome-feature
+    ```
+
+6. Open a pull request from yer branch and wait for the crew to review yer contributions.
+
+## License
+
+This project is licensed under the MIT License. For more information, see the [LICENSE](LICENSE) file in the project repository.
+
+---
+
+Fair winds and full bellies, adventurers! May yer forks be sharp and yer code be bug-free! 🍽️🎉
---
+++ index.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+// Welcome aboard the SS NomNomNavigator, sailing the digital seas in quest for scrumptious sustenance!
+const app = express();
+
+// Ahoy! The grand expedition to fetch the fabled menu data commences henceforth!
+async function fetchMenuData() {
+  // Hoist the sails, launch the browser! Steady as she goes, into the vast ocean of HTML!
+  const browser = await puppeteer.launch({ headless: true, headlessMode: 'new' });  // Stealth mode activated for clandestine operations!
+  const page = await browser.newPage();
+  
+  // The map leading to the treasure, bestowed upon by the environmental gods!
+  const restaurantURL = process.env.RESTAURANT_WEBSITE || 'https://default-restaurant-url.com';
+  
+  // Anchors aweigh! Set course for the Restaurant Isles!
+  await page.goto(restaurantURL);
+
+  // Behold, the lexicon of allergens! A guide for the gastronomically cautious.
+  const allergenMap = {
+    1: { name: 'Céréales contenant du gluten', emoji: '🌾' },
+    2: { name: 'Crustacés', emoji: '🦞' },
+    3: { name: 'Oeufs', emoji: '🥚' },
+    4: { name: 'Poissons', emoji: '🐟' },
+    5: { name: 'Arachides', emoji: '🥜' },
+    6: { name: 'Soja', emoji: '🌰' },
+    7: { name: 'Lait, y compris le lactose', emoji: '🥛' },
+    8: { name: 'Fruits à coque dure / noix', emoji: '🌰' },
+    9: { name: 'Céleri', emoji: '🌱' },
+    10: { name: 'Moutarde', emoji: '🌭' },
+    11: { name: 'Graines de sésame', emoji: '🌱' },
+    12: { name: 'Anhydride sulfureux / sulfites', emoji: '🧪' },
+    13: { name: 'Lupins', emoji: '🌱' },
+    14: { name: 'Mollusques', emoji: '🐚' }
+  };
+
+  // Venturing into the belly of the beast! The DOM! Where the menu data lurks in shadows.
+  const menuData = await page.evaluate((allergenMap) => {
+    const menuPlanTabs = document.querySelectorAll('.menu-plan-tabs > div');
+    const dateLabels = document.querySelectorAll('.day-nav .date');
+    const data = [];
+    const currentYear = new Date().getFullYear();  // The annals of time guide our journey!
+
+    menuPlanTabs.forEach((tab, tabIndex) => {
+      const dateSegments = dateLabels[tabIndex].textContent.trim().split('.');
+      const formattedDate = `${currentYear}-${dateSegments[1]}-${dateSegments[0]}`;  // By stars and calendar, we mark the date!
+      const menuItems = tab.querySelectorAll('.menu-item');
+      const meals = [];
+
+      menuItems.forEach(item => {
+        const itemContent = item.querySelector('.item-content');
+        const title = itemContent.querySelector('.menu-title').textContent.trim().replace(/\n/g, ' ');
+        const description = itemContent.querySelector('.menu-description').textContent.trim().replace(/\n/g, ' ');
+
+        const priceElement = itemContent.querySelector('.menu-prices .price .val');
+        const price = priceElement ? parseFloat(priceElement.textContent.trim()) : '';
+
+        const currencyElement = itemContent.querySelector('.menu-prices .price .desc');
+        const currency = currencyElement ? currencyElement.textContent.trim() : '';
+
+        const nutritionalInfoElement = itemContent.querySelector('.nutrition-table');
+        const nutritionalRows = nutritionalInfoElement ? nutritionalInfoElement.querySelectorAll('tr') : [];
+        const nutritionalInfo = Array.from(nutritionalRows).slice(1).map((row, rowIndex) => {
+          const cells = row.querySelectorAll('td');
+          return {
+            name: nutritionalRows[0].querySelectorAll('th')[rowIndex].textContent.trim(),
+            value: parseFloat(cells[rowIndex].textContent.trim()),
+            percentage: parseFloat(nutritionalRows[2].querySelectorAll('td')[rowIndex].textContent.trim())
+          };
+        });
+
+        const allergensElement = itemContent.querySelector('.allergen-info .allergen');
+        const allergenIndices = allergensElement ? allergensElement.innerText.trim().match(/\d+/g) : [];
+        const allergens = allergenIndices.map(index => {
+          const { name, emoji } = allergenMap[index.trim()];
+          return { id: index, name, emoji };
+        });
+
+        meals.push({
+          title,
+          description,
+          price,
+          currency,
+          allergens,
+          nutritionalInfo
+        });
+      });
+
+      data.push({
+        date: formattedDate,
+        meals
+      });
+    });
+
+    return data;  // Ah, the spoils of our brave endeavor!
+  }, allergenMap);  // The allergen map, our trusty companion in the DOM dungeon.
+
+  // Lower the drawbridge, the browser has served us well. Until the next quest!
+  await browser.close();
+  
+  return menuData;  // The cherished treasure, ready to be served to the hungry masses!
+}
+
+// The royal path to the bounteous banquet of data!
+app.get('/menu', async (req, res) => {
+  try {
+    const menuData = await fetchMenuData();
+    res.json(menuData);  // Behold, a feast for your API! Devour it with gusto!
+  } catch (err) {
+    console.error('Oh crumbs! Something went wrong:', err);
+    res.status(500).send('The kitchen is closed due to an orc invasion. Please try again later!');
+  }
+});
+
+// The grand hall opens its gates at port 3000! Let the digital feast commence!
+app.listen(3000, () => {
+  console.log('Server is simmering on port 3000');
+});
